197d5ff083f46696bbf807b5bb7f4580e054b593
Triantafillos <catlorddaffy@gmail.com>
2025-06-01 18:31:32 +0300

    Update env_stocktrading_portfolio_allocation.py

--- env_stocktrading_portfolio_allocation.py
+++ env_stocktrading_portfolio_allocation.py
@@ -108,8 +108,6 @@ class StockPortfolioEnv(gym.Env):
         # load data from a pandas dataframe
         self.covs_list = [col for col in self.df.columns.tolist() if "cov_" in col]
         self.features = self.tech_indicator_list + self.covs_list
-        # Normalize features
-        self.df[self.features] = (self.df[self.features] - self.df[self.features].mean()) / self.df[self.features].std()
         self.data = self.df.loc[self.day,:]
         
         self.state = self.data[self.features].to_numpy() # numpy array of 8x20 each day. 8 stocks, 20 features (indicators + 8 covariances)
